4e69050cd5992918aaaf226d131a503616c0b1c0
unknown <dnal03@msn.com>
2023-05-09 21:24:57 -0400

    Calculations

---
+++ src/Calculations.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculations {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Enter another number: ");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+
+        // Add the two numbers
+        int addedNum = firstNum + secondNum;
+        System.out.println("\n"+firstNum + " + " + secondNum + " = " + addedNum);
+
+        int subtractedNum = firstNum - secondNum;
+        System.out.println(firstNum + " - " + secondNum + " = " + subtractedNum);
+
+        int multiplyNum = firstNum * secondNum;
+        System.out.println(firstNum + " * " + secondNum + " = " + multiplyNum);
+
+        int divideNum = firstNum / secondNum;
+        System.out.println(firstNum + " / " + secondNum + " = " + divideNum);
+
+        int remainderNum = firstNum % secondNum;
+        System.out.println(firstNum + " % " + secondNum + " = " + remainderNum);
+
+        }
+
+    }
